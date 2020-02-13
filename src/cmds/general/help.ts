5ffe3998dedd59979e28fee7dc324ba977ece722
Minh Duc Vo <minhducsun2002@gmail.com>
2020-02-13 15:36:43 +0000

    Supercharging help command

--- src/cmds/general/help.ts
+++ src/cmds/general/help.ts
@@ -2,7 +2,11 @@ import { CommandHandler } from 'discord-akairo';
 import { Message, MessageEmbed } from 'discord.js';
 import { GeneralCommand } from './baseCommand';
 
+import { ERROR_COLOR, SUCCESS_COLOR } from '../../constants/colors';
+
 const commandName = 'help', aliases = [commandName];
+
+interface Arg { q: string };
 
 export = class extends GeneralCommand {
     constructor() {
@@ -10,22 +14,53 @@ export = class extends GeneralCommand {
             aliases,
             typing: true,
             description: 'You know nothing? This will help.',
+            args: [{
+                id: 'q',
+                type: 'string'
+            }]
         })
     }
 
-    async exec(m: Message) {
+    async exec(m: Message, { q } : Arg) {
         const handler = (this.client as any).cmdHandler as CommandHandler;
         // I am not supposed to do this, but yikes
-        let prefix = (handler.prefixes.size ? [...handler.prefixes.entries()][0][0] : handler.prefix) as string,
-            [main] = aliases;
-        let out = new MessageEmbed()
-            .setAuthor('', this.client.user.avatarURL())
-            .setDescription(
-                `${this.client.user} is ready to serve you.`
-                + `\nRun \`${prefix}${main} <category/command name>\` to see the respective help info.`
-                + `\nHere are the available categories :`
-                + `\`\`\`${[...handler.categories].map(([a]) => `* ${a}`).join('\n')}\`\`\``
-            )
+        let out : MessageEmbed | string = new MessageEmbed().setAuthor('', this.client.user.avatarURL()).setColor(SUCCESS_COLOR)
+
+        let prefix = (handler.prefixes.size ? [...handler.prefixes.entries()][0][0] : handler.prefix) as string;
+        if (q) {
+            // check if category name
+            if (handler.findCategory(q)) {
+                const cat = handler.findCategory(q);
+                out = out
+                    .setDescription(`The following command(s) belong to the **${cat.id}** category :`)
+                cat.forEach(({ description, aliases: [call] }) => out = (out as MessageEmbed).addField(
+                    `\`${prefix}${call}\``,
+                    description
+                ))
+            }
+            else if (handler.findCommand(q)) {
+                const cmd = handler.findCommand(q);
+                out = out
+                    .setTitle(`\`${prefix}${cmd.aliases[0]}\`${
+                        aliases.length > 1
+                        ? `\n(${aliases.slice(1).map(a => `\`${prefix}${a}\``).join(', ')})`
+                        : ''
+                    }`)
+                    .setDescription(cmd.description)
+            }
+            else out = `${m.author}, I couldn't find any category or command with the name of ${q}`
+        }
+        else {
+            let [main] = aliases;
+        
+            out = out
+                .setDescription(
+                    `${this.client.user} is ready to serve you.`
+                    + `\nRun \`${prefix}${main} <category/command name>\` to see the respective help info.`
+                    + `\nHere are the available categories :`
+                    + `\`\`\`${[...handler.categories].map(([a]) => `* ${a}`).join('\n')}\`\`\``
+                )
+        }
 
         m.channel.send(out);
     }
