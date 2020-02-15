747ef8e430930c2aec6d5a35df4bac658d6f1378
Minh Duc Vo <minhducsun2002@gmail.com>
2020-02-15 14:37:54 +0000

    Dropping experimental commands

--- src/cmds/experimental/baseCommand.ts
+++
@@ -1,6 +0,0 @@
-import { deriveBaseCommand } from '../../lib/classes/baseCommand';
-import { CommandOptions } from 'discord-akairo';
-
-export default (categoryName: string) => 
-    (opt: CommandOptions) =>
-        deriveBaseCommand(Object.assign({}, { category: `(Experimental) ${categoryName}` }, opt))
--- src/cmds/experimental/master/baseCommand.ts
+++
@@ -1,3 +0,0 @@
-import baseExp from '../baseCommand';
-
-export const FgoCommand = baseExp('F/GO')({})
