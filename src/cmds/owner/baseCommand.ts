828a6de3432d00d5d871ad30ba905f84d1f3f8e4
Minh Duc Vo <minhducsun2002@gmail.com>
2020-02-18 14:55:47 +0000

    Add notice for owner-only commands

--- src/cmds/owner/baseCommand.ts
+++ src/cmds/owner/baseCommand.ts
@@ -3,6 +3,9 @@ import { deriveBaseCommand } from '../../lib/classes/baseCommand';
 
 export const OwnerCommand = class extends deriveBaseCommand({ category: 'Reserved' }) {
     userPermissions = (m: Message) => {
-        return (this.client.ownerID as Array<String>).includes(m.author.id) ? null : 1
+        const pass = (this.client.ownerID as Array<String>).includes(m.author.id);
+        if (!pass)
+            m.channel.send(`${m.author}, this command can only be called by my owner, sorry for that.`)
+        return pass ? null : 1
     }
 }
