a2b23d7da78ba0ba74cdb84f33dc1691e07980c6
Minh Duc Vo <minhducsun2002@gmail.com>
2020-02-21 00:14:38 +0700

    Show arguments for commands when help

--- src/cmds/general/help.ts
+++ src/cmds/general/help.ts
@@ -1,9 +1,10 @@
-import { CommandHandler } from 'discord-akairo';
+import { CommandHandler, ArgumentOptions } from 'discord-akairo';
 import { Message, MessageEmbed } from 'discord.js';
 import { GeneralCommand } from './baseCommand';
 
 import { SUCCESS_COLOR } from '../../constants/colors';
 import Bot from '../..';
+import { BotCommand } from '../../lib/classes/baseCommand'
 
 const commandName = 'help', aliases = [commandName];
 
@@ -51,14 +52,35 @@ export = class extends GeneralCommand {
                 ))
             }
             else if (handler.findCommand(q)) {
-                const cmd = handler.findCommand(q);
+                const cmd = handler.findCommand(q) as BotCommand;
+                const mainCall = `${prefix}${cmd.aliases[0]}`, args = (cmd.args as ArgumentOptions[])
                 out = out
-                    .setTitle(`\`${prefix}${cmd.aliases[0]}\`${
+                    .setTitle(`\`${mainCall}\`${
                         (cmd.aliases.length > 1)
                         ? `\n(${cmd.aliases.slice(1).map(a => `\`${prefix}${a}\``).join(', ')})`
                         : ''
                     }`)
                     .setDescription(cmd.description)
+                    .addField(
+                        `Syntax`,
+                        `\`${mainCall} ${
+                            args.map(({ id, match, multipleFlags }) => `[${id}:${match}${multipleFlags ? '+' : ''}]`).join(' ')
+                        }\`\n\n${
+                            args.map(({ id, description }) => `- \`${id}\` ${description}`).join('\n')
+                        }`
+                    )
+                if (args.filter(a => a.match === 'flag' || a.match === 'option').length) {
+                    const f = args.filter(a => a.match === 'flag' || a.match === 'option');
+                    out = out.addField(
+                        `Flags`,
+                        f.map(({ flag, id }) => {
+                            if (!Array.isArray(flag)) flag = [flag];
+                            return `\`${id}\` **:** ${flag.map(a => `\`${a}\``).join(' ')} (+ content)`
+                        }).join('\n')
+                    )
+                }
+                if (args.filter(a => a.multipleFlags).length)
+                    out = out.setFooter(`+ means flag can be used multiple times.`)
             }
             else out = `${m.author}, I couldn't find any category or command with the name of ${q}`
         }
@@ -71,6 +93,18 @@ export = class extends GeneralCommand {
                     + `\nRun \`${prefix}${main} <category/command name>\` to see the respective help info.`
                     + `\nHere are the available categories :`
                     + `\`\`\`${[...handler.categories].map(([a]) => `* ${a}`).join('\n')}\`\`\``
+                )
+                .addField(
+                    'Syntax description',
+                    `A command's arguments are shown in the form of \`[name:type]\`.`
+                    + `\nThe following types are supported :\n`
+                    + 
+                    [
+                        [`phrase`, `matches by the order of the phrases inputted, ignoring flags/options.`],
+                        [`flag`, `matches specified phrases, evaluated to \`true\` if present, \`false\` otherwise.`],
+                        [`option`, 'as `flag`, but evaluated to the phrase after the flag.'],
+                        [`rest`, `matches the rest, ignoring flags/options, preserving whitespaces/quotes.`]
+                    ].map(([name, desc]) => `- \`${name}\` ${desc}`).join('\n')
                 )
         }
 
