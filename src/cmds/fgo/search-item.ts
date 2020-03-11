43d08d70a2ef2b58dfaf5db74ca9c45e96b5229d
Minh Duc Vo <minhducsun2002@gmail.com>
2020-03-11 18:20:55 +0700

    F/GO : Add item search

---
+++ src/cmds/fgo/search-item.ts
@@ -0,0 +1,46 @@
+import { Message, MessageEmbed } from 'discord.js';
+import { FgoCommand } from './baseCommand';
+import { constructQuery as c } from '../../lib/fgo/';
+import { ERROR_COLOR, SUCCESS_COLOR } from '../../constants/colors';
+import { log } from '../../lib/logger';
+import p from '../../lib/plural';
+
+const commandName = 'search-item';
+const aliases = [commandName, 'ssit']
+const MAX_ITEM = 25;
+interface a { q?: string }
+
+export = class extends FgoCommand {
+    constructor() {
+        super(commandName, {
+            aliases, 
+            args: [{
+                id: 'q',
+                match: 'rest'
+            }],
+            description: 'Search for items.',
+            typing: true
+        })
+    }
+
+    async exec(m: Message, { q }: a) {
+        const err = new MessageEmbed().setColor(ERROR_COLOR)
+            .setDescription(`Sorry, couldn't find anything that matched.`)
+        try {
+            let a = +q;
+            let _ = await c.mstItem(isNaN(a) ? { name: q } : { id: a }, MAX_ITEM + 1)
+                .NA.select('name id').exec();
+            
+            let out = new MessageEmbed()
+                .setColor(SUCCESS_COLOR)
+                .setTitle(`Found ${Math.min(MAX_ITEM, _.length)} item${p(_.length)}.`)
+                .setDescription(_.map(({ name, id }) => `\`${id}\` **${name}**`).join('\n'))
+            m.channel.send(out)
+        }
+        catch (e) {
+            let _ : Error = e;
+            log.error(`${_.name}: ${_.message}\n${_.stack}`)
+            m.channel.send(err.setDescription('Sorry, an error occurred.'))
+        }
+    }
+}
