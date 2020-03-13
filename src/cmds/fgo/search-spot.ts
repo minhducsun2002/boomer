eb6d05ab2bc573886925a34d46cbccb7bba7e0e8
Minh Duc Vo <minhducsun2002@gmail.com>
2020-03-13 18:50:19 +0700

    F/GO : use pagination for spot search

--- src/cmds/fgo/search-spot.ts
+++ src/cmds/fgo/search-spot.ts
@@ -3,11 +3,13 @@ import { FgoCommand } from './baseCommand';
 import { constructQuery as c } from '../../lib/fgo/';
 import { ERROR_COLOR, SUCCESS_COLOR } from '../../constants/colors';
 import { log } from '../../lib/logger';
+import { PagedEmbeds } from '@minhducsun2002/paged-embeds';
 import p from '../../lib/plural';
+import { chunk } from '../../lib/chunk';
 
 const commandName = 'search-spot';
 const aliases = [commandName, 'ssp']
-const MAX_QUEST = 25;
+const MAX_PAGE = 15;
 interface a { q?: string }
 
 export = class extends FgoCommand {
@@ -28,14 +30,40 @@ export = class extends FgoCommand {
             .setDescription(`Sorry, couldn't find anything that matched.`)
         try {
             let a = +q;
-            let _ = await c.mstSpot(isNaN(a) ? { name: q } : { id: a }, MAX_QUEST + 1)
+            let _ = await c.mstSpot(isNaN(a) ? { name: q } : { id: a }, 0)
                 .NA.select('name id').exec();
             
-            let out = new MessageEmbed()
-                .setColor(SUCCESS_COLOR)
-                .setTitle(`Found ${Math.min(MAX_QUEST, _.length)} spot${p(_.length)}.`)
-                .setDescription(_.map(({ name, id }) => `\`${id}\` **${name}**`).join('\n'))
-            m.channel.send(out)
+            if (_.length > MAX_PAGE)
+                new PagedEmbeds()
+                    .setChannel(m.channel)
+                    .setEmbeds(
+                        chunk(_, MAX_PAGE)
+                            .map((s, i, c) => new MessageEmbed()
+                                .setTitle(`Found ${_.length} spot${p(_.length)}.`)
+                                .setDescription(s.map(({ name, id }) => `\`${id}\` **${name}**`).join('\n'))
+                                .setFooter(`Page ${i + 1}/${c.length}`)
+                            )
+                    )
+                    .addHandler(this.client.emojis.resolveIdentifier('⬅️'), (m, i, u, e, c) => {
+                        return {
+                            index: (i - 1 + e.length) % e.length,
+                            embed: e
+                        }
+                    })
+                    .addHandler(this.client.emojis.resolveIdentifier('➡️'), (m, i, u, e, c) => {
+                        return {
+                            index: (i + 1 + e.length) % e.length,
+                            embed: e
+                        }
+                    })
+                    .run({ idle: 20000, dispose: true })
+            else
+                m.channel.send(
+                    '',
+                    new MessageEmbed()
+                        .setTitle(`Found ${_.length} spot${p(_.length)}.`)
+                        .setDescription(_.map(({ name, id }) => `\`${id}\` **${name}**`).join('\n'))
+                )
         }
         catch (e) {
             let _ : Error = e;
