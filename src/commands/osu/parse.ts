0f0c5e39005694a5680e950a741767c4622ccdb9
Minh Duc Vo <minhducsun2002@gmail.com>
2020-05-27 13:30:38 +0700

    osu! : integrate pp calculator into replay parser

--- src/commands/osu/parse.ts
+++ src/commands/osu/parse.ts
@@ -1,10 +1,11 @@
 import { MessageEmbed, Message } from 'discord.js';
 import axios from 'axios';
+import ojsama from 'ojsama';
 import { OsuCommand } from './baseCommand';
 import { ERROR_COLOR, SUCCESS_COLOR } from '../../constants/colors';
 import { Replay } from '@minhducsun2002/node-osr-parser';
 import { mode_friendly } from './beatmap';
-import { modToString } from '../../lib/osu/utils';
+import { modToString, accuracy } from '../../lib/osu/utils';
 import Bot from '../../';
 
 const commandName = 'parse';
@@ -16,12 +17,18 @@ export = class extends OsuCommand {
         super(commandName, {
             aliases,
             description: 'Parse a replay file',
-            cooldown: 5000
-            // 5 secs
+            cooldown: 5000,
+            // 5 secs,
+            args: [{
+                id: 'pp',
+                type: 'flag',
+                description: 'Whether to calculate pp for this replay.',
+                flag: ['/pp']
+            }]
         })
     }
 
-    async exec(m : Message) {
+    async exec(m : Message, { pp } = { pp: false }) {
         const err = new MessageEmbed().setColor(ERROR_COLOR)
             .setDescription(`Sorry, could not parse your replay.`)
         let _ = m.attachments.array().filter(a => a.name.endsWith('.osr'));
@@ -38,7 +45,7 @@ export = class extends OsuCommand {
         } catch {
             return m.channel.send(err);
         }
-        let { player, timestamp, score, gamemode, maxCombo, perfect, mods, version, md5map } = replay as Replay;
+        let { player, timestamp, score, gamemode, maxCombo, perfect, mods, version, md5map, accuracies } = replay as Replay;
         let _m = modToString(mods), inline = true;
         await m.channel.send(
             new MessageEmbed()
@@ -46,9 +53,11 @@ export = class extends OsuCommand {
                 .setAuthor(`osu! version ${version}`)
                 .setTitle(`${mode_friendly[gamemode]} replay by **${player}**`)
                 .addFields([
-                    { name: 'Score', value: `${score} - **${maxCombo}**x ${perfect ? '(Perfect)' : ''}`, inline },
+                    { name: 'Score', value: `${score}\n**${
+                        (100 * accuracy[gamemode as keyof typeof accuracy](accuracies)).toFixed(3)
+                    }**% - **${maxCombo}**x ${perfect ? '(Perfect)' : ''}`, inline },
                     { name: 'Timestamp', value: timestamp.toLocaleString('en-US'), inline },
-                    { name: 'Mods', value: _m.join(', ') || 'None' },
+                    { name: 'Mods', value: _m.join(', ') || 'None', inline }
                 ])
         );
 
@@ -58,9 +67,23 @@ export = class extends OsuCommand {
                 let __ = await axios.get(`https://osu.ppy.sh/api/get_beatmaps?k=${key}&h=${md5map}`, { responseType: 'json' })
                 if (__.data.length) {
                     let _ = (this.client as Bot).cmdHandler;
-                    _.runCommand(m, _.findCommand(`beatmap`), { beatmap: __.data[0].beatmap_id })
+                    _.runCommand(m, _.findCommand(`beatmap`), { beatmap: __.data[0].beatmap_id });
+                    if (pp) {
+                        _.runCommand(
+                            m, 
+                            _.findCommand(`pp`),
+                            {
+                                beatmap: __.data[0].beatmap_id,
+                                accuracy: (accuracy[gamemode as keyof typeof accuracy](accuracies) * 100).toFixed(4),
+                                mods: ojsama.modbits.string(mods),
+                                combo: maxCombo
+                            }
+                        );
+                    }
                 }
             } catch {}
         }
+
+        
     }
 }
