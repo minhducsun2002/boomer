87df14670f85308407711343beb38702147d343a
Minh Duc Vo <minhducsun2002@gmail.com>
2020-07-04 23:34:33 +0700

    Fix reloading commands display incorrect time

--- src/commands/owner/reloadCommand.ts
+++ src/commands/owner/reloadCommand.ts
@@ -1,12 +1,10 @@
-import { MessageEmbed, Message } from 'discord.js';
+import { Message } from 'discord.js';
 import { OwnerCommand } from './baseCommand';
 import { performance, PerformanceObserver } from 'perf_hooks';
 import { Command } from 'discord-akairo';
 
 const commandName = 'reload-cmd'
 const aliases = [commandName, 'reload', 'reloadcmd', 'rld-cmd']
-
-interface arg { cmd?: Command }
 
 export = class extends OwnerCommand {
     constructor() {
@@ -20,12 +18,12 @@ export = class extends OwnerCommand {
         })
     }
 
-    async exec(m: Message, { cmd } : arg) {
+    async exec(m: Message, { cmd } : { cmd?: Command }) {
         const handler = this.client.commandHandler;
         if (cmd) {
             let out = await m.channel.send(`Reloading \`${cmd.id}\`...`);
             new PerformanceObserver((l, o) => {
-                out.edit(`Reloaded \`${cmd.id}\` in \`${l.getEntries()[0].duration}\`s.`)
+                out.edit(`Reloaded \`${cmd.id}\` in \`${l.getEntries()[0].duration / 1000}\`s.`)
                 o.disconnect();
             }).observe({ entryTypes: ['function'] });
             performance.timerify(() => handler.reload(cmd.id))()
