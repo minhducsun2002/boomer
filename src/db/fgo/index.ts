ca4f16e0c4d3c7d073d23acd0b7377e9cc208115
Minh Duc Vo <minhducsun2002@gmail.com>
2020-08-15 18:32:36 +0700

    F/GO : Refactor DB initialization

--- src/db/fgo/index.ts
+++ src/db/fgo/index.ts
@@ -1,7 +1,8 @@
-import { createConnection, Connection, Model } from 'mongoose';
+import { createConnection } from 'mongoose';
 import mongoose from 'mongoose';
 import { componentLog } from '@pepper/utils';
 import cfg from '../../config';
+import { initializeMasterModels as _, initializeServantModel } from './models';
 
 require('mongoose-cache').install(mongoose, { max: 300, maxAge: 1000 * 60 * 60 * 30 }, mongoose.Aggregate)
 
@@ -24,62 +25,6 @@ const master = {
         .on('open', () => log.success(a(`Successfully connected to master (JP) database.`)))
 }
 
-const connections = new Map<string, Connection>()
-    .set('main', main)
-    .set('master_NA', master.NA)
-    .set('master_JP', master.JP)
-
-// models
-import { Servant, ServantSchema } from './model';
-import { mstSvtSchema, mstSvtDocument } from './master/mstSvt';
-import { mstClassSchema, mstClassDocument } from './master/mstClass';
-import { mstAttriRelationSchema, mstAttriRelationDocument } from './master/mstAttriRelation';
-import { mstQuestSchema, mstQuestDocument } from './master/mstQuest';
-import { mstSpotSchema, mstSpotDocument } from './master/mstSpot';
-import { mstWarSchema, mstWarDocument } from './master/mstWar';
-import { mstSvtCardSchema,  mstSvtCardDocument } from './master/mstSvtCard';
-import { mstSvtCommentSchema,  mstSvtCommentDocument } from './master/mstSvtComment';
-import { mstSvtSkillSchema,  mstSvtSkillDocument } from './master/mstSvtSkill';
-import { mstSkillDetailSchema,  mstSkillDetailDocument } from './master/mstSkillDetail';
-import { mstItemSchema,  mstItemDocument } from './master/mstItem';
-import { mstQuestConsumeItemSchema,  mstQuestConsumeItemDocument } from './master/mstQuestConsumeItem';
-import { mstQuestPhaseSchema, mstQuestPhaseDocument } from './master/mstQuestPhase';
-import { mstSvtLimitSchema, mstSvtLimitDocument } from './master/mstSvtLimit';
-import { mstSvtTreasureDeviceSchema, mstSvtTreasureDeviceDocument } from './master/mstSvtTreasureDevice';
-import { mstTreasureDeviceLvSchema, mstTreasureDeviceLvDocument } from './master/mstTreasureDeviceLv';
-import { mstTreasureDeviceSchema, mstTreasureDeviceDocument } from './master/mstTreasureDevice';
-import { mstFuncSchema, mstFuncDocument } from './master/mstFunc';
-import { mstBuffSchema, mstBuffDocument } from './master/mstBuff';
-import { mstSkillSchema, mstSkillDocument } from './master/mstSkill';
-import { mstSkillLvSchema, mstSkillLvDocument } from './master/mstSkillLv';
-
-export const ServantModel : Model<Servant> = connections.get('main').model('Servant', ServantSchema)
-
-const _ = (conn : Connection) => {
-    return {
-        mstSvt: conn.model('mstSvt', mstSvtSchema, 'mstSvt') as Model<mstSvtDocument>,
-        mstClass: conn.model('mstClass', mstClassSchema, 'mstClass') as Model<mstClassDocument>,
-        mstAttriRelation: conn.model('mstAttriRelation', mstAttriRelationSchema, 'mstAttriRelation') as Model<mstAttriRelationDocument>,
-        mstQuest: conn.model('mstQuest', mstQuestSchema, 'mstQuest') as Model<mstQuestDocument>,
-        mstSpot: conn.model('mstSpot', mstSpotSchema, 'mstSpot') as Model<mstSpotDocument>,
-        mstWar: conn.model('mstWar', mstWarSchema, 'mstWar') as Model<mstWarDocument>,
-        mstSvtCard: conn.model('mstSvtCard', mstSvtCardSchema, 'mstSvtCard') as Model<mstSvtCardDocument>,
-        mstSvtComment: conn.model('mstSvtComment', mstSvtCommentSchema, 'mstSvtComment') as Model<mstSvtCommentDocument>,
-        mstSvtSkill: conn.model('mstSvtSkill', mstSvtSkillSchema, 'mstSvtSkill') as Model<mstSvtSkillDocument>,
-        mstSkillDetail: conn.model('mstSkillDetail', mstSkillDetailSchema, 'mstSkillDetail') as Model<mstSkillDetailDocument>,
-        mstItem: conn.model('mstItem', mstItemSchema, 'mstItem') as Model<mstItemDocument>,
-        mstQuestConsumeItem: conn.model('mstQuestConsumeItem', mstQuestConsumeItemSchema, 'mstQuestConsumeItem') as Model<mstQuestConsumeItemDocument>,
-        mstQuestPhase: conn.model('mstQuestPhase', mstQuestPhaseSchema, 'mstQuestPhase') as Model<mstQuestPhaseDocument>,
-        mstSvtLimit: conn.model('mstSvtLimit', mstSvtLimitSchema, 'mstSvtLimit') as Model<mstSvtLimitDocument>,
-        mstSvtTreasureDevice: conn.model('mstSvtTreasureDevice', mstSvtTreasureDeviceSchema, 'mstSvtTreasureDevice') as Model<mstSvtTreasureDeviceDocument>,
-        mstTreasureDeviceLv: conn.model('mstTreasureDeviceLv', mstTreasureDeviceLvSchema, 'mstTreasureDeviceLv') as Model<mstTreasureDeviceLvDocument>,
-        mstTreasureDevice: conn.model('mstTreasureDevice', mstTreasureDeviceSchema, 'mstTreasureDevice') as Model<mstTreasureDeviceDocument>,
-        mstFunc: conn.model('mstFunc', mstFuncSchema, 'mstFunc') as Model<mstFuncDocument>,
-        mstBuff: conn.model('mstBuff', mstBuffSchema, 'mstBuff') as Model<mstBuffDocument>,
-        mstSkill: conn.model('mstSkill', mstSkillSchema, 'mstSkill') as Model<mstSkillDocument>,
-        mstSkillLv: conn.model('mstSkillLv', mstSkillLvSchema, 'mstSkillLv') as Model<mstSkillLvDocument>,
-    }
-}
-
+export const ServantModel = initializeServantModel(main);
 export const NA = _(master.NA), JP = _(master.JP)
 export type DBInstance = ReturnType<typeof _>;
---
+++ src/db/fgo/models.ts
@@ -0,0 +1,51 @@
+import type { Connection, Model } from 'mongoose';
+
+import { Servant, ServantSchema } from './main';
+
+import { mstSvtSchema, mstSvtDocument } from './master/mstSvt';
+import { mstClassSchema, mstClassDocument } from './master/mstClass';
+import { mstAttriRelationSchema, mstAttriRelationDocument } from './master/mstAttriRelation';
+import { mstQuestSchema, mstQuestDocument } from './master/mstQuest';
+import { mstSpotSchema, mstSpotDocument } from './master/mstSpot';
+import { mstWarSchema, mstWarDocument } from './master/mstWar';
+import { mstSvtCardSchema,  mstSvtCardDocument } from './master/mstSvtCard';
+import { mstSvtCommentSchema,  mstSvtCommentDocument } from './master/mstSvtComment';
+import { mstSvtSkillSchema,  mstSvtSkillDocument } from './master/mstSvtSkill';
+import { mstSkillDetailSchema,  mstSkillDetailDocument } from './master/mstSkillDetail';
+import { mstItemSchema,  mstItemDocument } from './master/mstItem';
+import { mstQuestConsumeItemSchema,  mstQuestConsumeItemDocument } from './master/mstQuestConsumeItem';
+import { mstQuestPhaseSchema, mstQuestPhaseDocument } from './master/mstQuestPhase';
+import { mstSvtLimitSchema, mstSvtLimitDocument } from './master/mstSvtLimit';
+import { mstSvtTreasureDeviceSchema, mstSvtTreasureDeviceDocument } from './master/mstSvtTreasureDevice';
+import { mstTreasureDeviceLvSchema, mstTreasureDeviceLvDocument } from './master/mstTreasureDeviceLv';
+import { mstTreasureDeviceSchema, mstTreasureDeviceDocument } from './master/mstTreasureDevice';
+import { mstFuncSchema, mstFuncDocument } from './master/mstFunc';
+import { mstBuffSchema, mstBuffDocument } from './master/mstBuff';
+import { mstSkillSchema, mstSkillDocument } from './master/mstSkill';
+import { mstSkillLvSchema, mstSkillLvDocument } from './master/mstSkillLv';
+
+export const initializeMasterModels = (conn : Connection) => ({
+    mstSvt: conn.model('mstSvt', mstSvtSchema, 'mstSvt') as Model<mstSvtDocument>,
+    mstClass: conn.model('mstClass', mstClassSchema, 'mstClass') as Model<mstClassDocument>,
+    mstAttriRelation: conn.model('mstAttriRelation', mstAttriRelationSchema, 'mstAttriRelation') as Model<mstAttriRelationDocument>,
+    mstQuest: conn.model('mstQuest', mstQuestSchema, 'mstQuest') as Model<mstQuestDocument>,
+    mstSpot: conn.model('mstSpot', mstSpotSchema, 'mstSpot') as Model<mstSpotDocument>,
+    mstWar: conn.model('mstWar', mstWarSchema, 'mstWar') as Model<mstWarDocument>,
+    mstSvtCard: conn.model('mstSvtCard', mstSvtCardSchema, 'mstSvtCard') as Model<mstSvtCardDocument>,
+    mstSvtComment: conn.model('mstSvtComment', mstSvtCommentSchema, 'mstSvtComment') as Model<mstSvtCommentDocument>,
+    mstSvtSkill: conn.model('mstSvtSkill', mstSvtSkillSchema, 'mstSvtSkill') as Model<mstSvtSkillDocument>,
+    mstSkillDetail: conn.model('mstSkillDetail', mstSkillDetailSchema, 'mstSkillDetail') as Model<mstSkillDetailDocument>,
+    mstItem: conn.model('mstItem', mstItemSchema, 'mstItem') as Model<mstItemDocument>,
+    mstQuestConsumeItem: conn.model('mstQuestConsumeItem', mstQuestConsumeItemSchema, 'mstQuestConsumeItem') as Model<mstQuestConsumeItemDocument>,
+    mstQuestPhase: conn.model('mstQuestPhase', mstQuestPhaseSchema, 'mstQuestPhase') as Model<mstQuestPhaseDocument>,
+    mstSvtLimit: conn.model('mstSvtLimit', mstSvtLimitSchema, 'mstSvtLimit') as Model<mstSvtLimitDocument>,
+    mstSvtTreasureDevice: conn.model('mstSvtTreasureDevice', mstSvtTreasureDeviceSchema, 'mstSvtTreasureDevice') as Model<mstSvtTreasureDeviceDocument>,
+    mstTreasureDeviceLv: conn.model('mstTreasureDeviceLv', mstTreasureDeviceLvSchema, 'mstTreasureDeviceLv') as Model<mstTreasureDeviceLvDocument>,
+    mstTreasureDevice: conn.model('mstTreasureDevice', mstTreasureDeviceSchema, 'mstTreasureDevice') as Model<mstTreasureDeviceDocument>,
+    mstFunc: conn.model('mstFunc', mstFuncSchema, 'mstFunc') as Model<mstFuncDocument>,
+    mstBuff: conn.model('mstBuff', mstBuffSchema, 'mstBuff') as Model<mstBuffDocument>,
+    mstSkill: conn.model('mstSkill', mstSkillSchema, 'mstSkill') as Model<mstSkillDocument>,
+    mstSkillLv: conn.model('mstSkillLv', mstSkillLvSchema, 'mstSkillLv') as Model<mstSkillLvDocument>,
+})
+
+export const initializeServantModel = (c : Connection) => c.model('Servant', ServantSchema) as Model<Servant>
