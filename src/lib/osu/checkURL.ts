fe0aaab435f65402aa0574282367b1f256b88d65
minhducsun2002 <minhducsun2002@gmail.com>
2020-11-04 15:46:51 +0700

    osu! : Fix matching for beatmapsets/id#mode/id URLs

--- src/lib/osu/checkURL.ts
+++ src/lib/osu/checkURL.ts
@@ -5,7 +5,7 @@ export function checkURL (url : string) : { set?: number; id?: number; mode?: st
     let id = +(
         url.match(/http(?:s)*:\/\/osu\.ppy\.sh\/beatmaps\/(\d+)/)
         || url.match(/http(?:s)*:\/\/osu\.ppy\.sh\/b\/(\d+)/)
-        || (url.match(/http(?:s)*:\/\/osu\.ppy\.sh\/beatmapsets\/(\d+)#(osu|taiko|fruits|mania)\/(\d+)/) || []).slice(3)
+        || (url.match(/http(?:s)*:\/\/osu\.ppy\.sh\/beatmapsets\/(\d+)#(osu|taiko|fruits|mania)\/(\d+)/) || []).slice(2)
     ).slice(1)[0];
     let [mode] = (url.match(/http(?:s)*:\/\/osu\.ppy\.sh\/beatmapsets\/(\d+)#(osu|taiko|fruits|mania)\/(\d+)/) || []).slice(2)
     return { set, id, mode };
