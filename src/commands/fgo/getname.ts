425929e8a2299716ce8ded4faaf291293f2b415a
minhducsun2002 <32769878+minhducsun2002@users.noreply.github.com>
2020-12-16 00:32:05 +0700

    F/GO : Check for valid servant alias before listing names

--- src/commands/fgo/getname.ts
+++ src/commands/fgo/getname.ts
@@ -1,9 +1,7 @@
 import { Message, MessageEmbed } from 'discord.js';
 import { FgoCommand } from './baseCommand';
 import { ERROR_COLOR, SUCCESS_COLOR } from '@pepper/constants/colors';
-import { SvtType } from '@pepper/constants/fgo';
 import search from '@pepper/modules/fgo/servant-name-search';
-import master from '@pepper/modules/fgo/master-data';
 
 const commandName = 'getname';
 const aliases = [commandName, 'getnames'];
@@ -15,38 +13,32 @@ export = class extends FgoCommand {
             description: `List aliases for a servant.`,
             typing: true,
             args: [{
-                id: 'collectionNo',
+                id: 'query',
                 match: 'phrase',
-                type: 'number',
-                description: 'Servant ID to list aliases.'
+                type: 'string',
+                description: 'Search query for a servant. Could be an alias or a servant ID.'
             }]
         })
     }
 
-    async exec(m : Message, { collectionNo } : { collectionNo: number }) {
+    async exec(m : Message, { query } : { query: string }) {
         const err = new MessageEmbed().setColor(ERROR_COLOR);
         let bail = (s : string) => m.channel.send(err.setDescription(s));
-        let search_instance = this.client.moduleHandler.findInstance(search),
-            master_data = this.client.moduleHandler.findInstance(master)
+        let search_instance = this.client.moduleHandler.findInstance(search)
 
-        let verify = async () => await master_data.JP.mstSvt.findOne({
-            $or: [
-                { type: SvtType.HEROINE, collectionNo },
-                { type: SvtType.NORMAL, collectionNo },
-                { type: SvtType.ENEMY_COLLECTION_DETAIL, collectionNo }
-            ]
-        }).exec();
+        let servantId = +query;
+        if (isNaN(servantId)) servantId = (await search_instance.getAlias(query))?.collectionNo;
         
-        if (!(Number.isSafeInteger(collectionNo) && (await verify())))
-            return bail(`Please specify a valid servant ID.`)
+        if (!Number.isSafeInteger(servantId))
+            return bail(`Please specify a valid servant ID or alias.`)
 
-        let res = await search_instance.listAlias(collectionNo);
+        let res = await search_instance.listAlias(servantId);
         if (!res.length)
-            return bail(`There's no custom alias for servant ID ${collectionNo}.`);
+            return bail(`There's no custom alias for servant ${query}.`);
         else
             m.channel.send(
                 new MessageEmbed()
-                    .setTitle(`Custom alias${res.length > 1 ? 'es' : ''} for servant ID ${collectionNo}`)
+                    .setTitle(`Custom alias${res.length > 1 ? 'es' : ''} for servant ID ${servantId}`)
                     .setDescription(res.map(_ => `- \`${_.alias}\``).join('\n'))
                     .setColor(SUCCESS_COLOR)
             )
