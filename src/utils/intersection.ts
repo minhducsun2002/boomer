4a08cd76a725c0b61d412b1a6f75cc7956d94354
minhducsun2002 <32769878+minhducsun2002@users.noreply.github.com>
2021-03-03 17:39:27 +0700

    Fix set intersection algorithm always returning the smallest set

--- src/utils/intersection.ts
+++ src/utils/intersection.ts
@@ -7,7 +7,7 @@ export function findSetIntersection<T>(...sets: Set<T>[]) : Set<T> {
     // check for index 
     // set at which is smallest 
     let leastIndex = sets.reduce((last, curr, i) => curr.size < sets[last].size ? i : last, 0);
-    let [base] = sets.splice(leastIndex);
+    let [base] = sets.splice(leastIndex, 1);
     let out = new Set<T>();
     for (let _ of base)
         sets.every(s => s.has(_)) ? out.add(_) : 0;
