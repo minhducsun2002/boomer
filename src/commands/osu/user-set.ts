8b3cafd5ffd14ca0a4d74b7cd7f98584c3813146
minhducsun2002 <32769878+minhducsun2002@users.noreply.github.com>
2021-03-10 17:52:50 +0700

    osu! : Add command to bind users to osu! usernames

---
+++ src/commands/osu/user-set.ts
@@ -0,0 +1,39 @@
+import { OsuCommand } from './baseCommand';
+import { Message } from 'discord.js';
+import UsernameDb from '@pepper/modules/osu/username-db';
+
+const commandName = 'user-set';
+const aliases = [commandName, 'set'];
+
+export = class extends OsuCommand {
+    constructor() {
+        super(commandName, {
+            aliases,
+            description: 'Set your osu! username with this command',
+            locked: true,
+            args: [{
+                id: 'username',
+                match: 'rest',
+                description: 'Username to (un)set'
+            }, {
+                id: 'unset',
+                match: 'flag',
+                flag: ['/unset'],
+                description: 'Unset the username'
+            }]
+        })
+    }
+
+    async exec(m : Message, { username, unset } : { username: string, unset: boolean }) {
+        let userDb = this.client.moduleHandler.findInstance(UsernameDb);
+        if (unset) {
+            let record = await userDb.unsetUser(m.author.id);
+            return m.channel.send(`${m.author.toString()} is no longer bound to ${
+                record ? `username **${record.osuUsername}**` : `any username`
+            }.`);
+        }
+
+        await userDb.setUser(m.author.id, username);
+        return m.channel.send(`${m.author.toString()} is now bound to username **${username}**.`);
+    }
+}
---
+++ src/modules/osu/username-db.ts
@@ -0,0 +1,41 @@
+import { componentLog } from '@pepper/utils';
+import { OsuModule } from "./base";
+import { createConnection, Document, Model, Schema } from 'mongoose';
+
+interface UsernameRecord extends Document {
+    osuUsername: string;
+    discordUserId: string;
+}
+
+export = class extends OsuModule {
+    private log = new componentLog('osu! username database');
+
+    constructor() {
+        super(`username-db`);
+    }
+
+    private model : Model<UsernameRecord, {}>;
+
+    async setUser(userId : string, username: string) {
+        return await this.model.findOneAndUpdate(
+            { osuUsername: username, discordUserId: userId },
+            { osuUsername: username, discordUserId: userId },
+            { upsert: true }
+        );
+    }
+    async unsetUser(userId : string) {
+        return await this.model.findOneAndDelete({ discordUserId: userId });
+    }
+    async getUser(userId : string) {
+        return await this.model.findOne({ discordUserId: userId });
+    }
+
+    async initialize() {
+        let { osu } = this.client.config.database;
+        let { username } = osu as Exclude<typeof osu, string>;
+
+        this.model = createConnection(`${username}`)
+            .on('open', () => this.log.success(`Connected to osu! username database.`))
+            .model('username', new Schema({ osuUsername: String, discordUserId: String }), 'username');
+    }
+}
