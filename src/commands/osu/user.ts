395718562d3e8da17245b02001e22b76984396fd
minhducsun2002 <32769878+minhducsun2002@users.noreply.github.com>
2021-03-23 19:08:58 +0700

    osu! : Unwrap user view code from try-catch clause

--- src/commands/osu/user.ts
+++ src/commands/osu/user.ts
@@ -35,59 +35,54 @@ export default class extends OsuCommand {
         if (!user)
             return m.channel.send(err.setDescription(`Who do you want to search for?`));
 
-        try {
-            let userdata = await fetchUser(user, mode);
-            let {
-                username, country: { code: cc }, avatar_url, join_date, id,
-                statistics: {
-                    pp, rank, global_rank, play_count, play_time,
-                    maximum_combo, hit_accuracy,
-                    grade_counts: { ss, ssh, s, sh, a }
-                }
-            } = userdata.user;
-            let [score] = userdata.extra.scoresBest;
-            let [_w, _d, _h, _m, _s] = [
-                Math.floor((play_time / (3600 * 24 * 7))),
-                Math.floor((play_time % (3600 * 24 * 7)) / 86400),
-                Math.floor((play_time % 86400) / 3600),
-                Math.floor((play_time % 3600) / 60),
-                Math.floor(play_time % 60)
-            ]
+        let userdata = await fetchUser(user, mode);
 
-            let out = new MessageEmbed()
-                .setTitle(username)
-                .setURL(`https://osu.ppy.sh/users/${id}`)
-                .setThumbnail(avatar_url.startsWith(`https://`) ? avatar_url : null)
-                .setColor(SUCCESS_COLOR)
-                .setDescription(
-                    (global_rank
-                        ? `**${pp}**pp (${this.resolveEarthEmoji(cc)} #**${global_rank}** | :flag_${cc.toLowerCase()}: #**${rank.country}**)`
-                        : `Unranked`)
-                    + `.\n**${(+hit_accuracy).toFixed(3)}%** accuracy - **${maximum_combo}**x max combo.`
-                )
-                .addField('Play ranks', `**${ssh}** XH | **${ss}** X\n**${sh}** SH | **${s}** S\n**${a}** A`, true)
-                .addField(`Play count`,`${play_count} times\n${_w}w ${_d}d ${_h}h ${_m}m ${_s}s`, true)
-                .setFooter(`Joined ${new Date(join_date).toLocaleString('vi-VN', { timeZone: 'UTC' })} UTC.`);
-
-            if (score) {
-                let { beatmapset, beatmap, perfect, mods } = score,
-                    { ar, drain, difficulty_rating, accuracy, cs } = beatmap;
-                out.addField(
-                    `Best performance`,
-                    `[**${score.rank}**] **${score.pp}**pp (**${
-                        (score.accuracy * 100).toFixed(3)
-                    }**% | **${score.max_combo}**x)` + (perfect ? ' (FC)' : '')
-                    + `\n[${beatmapset.artist} - ${beatmapset.title} [${beatmap.version}]](https://osu.ppy.sh/beatmaps/${beatmap.id})`
-                    + (mods.length ? `+${mods.join('')}` : '')
-                    + `\n${difficulty_rating} :star: - \`AR\`**${ar}** \`CS\`**${cs}** \`OD\`**${accuracy}** \`HP\`**${drain}**`
-                )
+        let {
+            username, country: { code: cc }, avatar_url, join_date, id,
+            statistics: {
+                pp, rank, global_rank, play_count, play_time,
+                maximum_combo, hit_accuracy,
+                grade_counts: { ss, ssh, s, sh, a }
             }
+        } = userdata.user;
+        let [score] = userdata.extra.scoresBest;
+        let [_w, _d, _h, _m, _s] = [
+            Math.floor((play_time / (3600 * 24 * 7))),
+            Math.floor((play_time % (3600 * 24 * 7)) / 86400),
+            Math.floor((play_time % 86400) / 3600),
+            Math.floor((play_time % 3600) / 60),
+            Math.floor(play_time % 60)
+        ]
 
-            m.channel.send(out)
+        let out = new MessageEmbed()
+            .setTitle(username)
+            .setURL(`https://osu.ppy.sh/users/${id}`)
+            .setThumbnail(avatar_url.startsWith(`https://`) ? avatar_url : null)
+            .setColor(SUCCESS_COLOR)
+            .setDescription(
+                (global_rank
+                    ? `**${pp}**pp (${this.resolveEarthEmoji(cc)} #**${global_rank}** | :flag_${cc.toLowerCase()}: #**${rank.country}**)`
+                    : `Unranked`)
+                + `.\n**${(+hit_accuracy).toFixed(3)}%** accuracy - **${maximum_combo}**x max combo.`
+            )
+            .addField('Play ranks', `**${ssh}** XH | **${ss}** X\n**${sh}** SH | **${s}** S\n**${a}** A`, true)
+            .addField(`Play count`,`${play_count} times\n${_w}w ${_d}d ${_h}h ${_m}m ${_s}s`, true)
+            .setFooter(`Joined ${new Date(join_date).toLocaleString('vi-VN', { timeZone: 'UTC' })} UTC.`);
+
+        if (score) {
+            let { beatmapset, beatmap, perfect, mods } = score,
+                { ar, drain, difficulty_rating, accuracy, cs } = beatmap;
+            out.addField(
+                `Best performance`,
+                `[**${score.rank}**] **${score.pp}**pp (**${
+                    (score.accuracy * 100).toFixed(3)
+                }**% | **${score.max_combo}**x)` + (perfect ? ' (FC)' : '')
+                + `\n[${beatmapset.artist} - ${beatmapset.title} [${beatmap.version}]](https://osu.ppy.sh/beatmaps/${beatmap.id})`
+                + (mods.length ? `+${mods.join('')}` : '')
+                + `\n${difficulty_rating} :star: - \`AR\`**${ar}** \`CS\`**${cs}** \`OD\`**${accuracy}** \`HP\`**${drain}**`
+            )
         }
-        catch (e) {
-            console.log(e)
-            m.channel.send(err.setDescription(`Sorry, an error occurred.`));
-        }
+
+        m.channel.send(out)
     }
 }
